8c68154b6755d33519af25e7d2b093e5d06632fd
Francesc711 <francesc.marti@uca.ac.uk>
2025-09-14 16:53:35 +0800

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let ws = new WebSocket("wss://sose-z1oj.onrender.com/:443");
 let controllTD = document.querySelector('.controllTD') ;
 controllTD.addEventListener('input', (event) => {
   ws.send(JSON.stringify({ 
-  'slider1': controllTD2.value / 100,
+  'sliderr1': controllTD2.value / 100,
   'ID': 22 
 }));
 }, false);
